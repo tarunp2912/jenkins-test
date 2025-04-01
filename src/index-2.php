a2c089f456a46a3329bce5b9c48dd0bb15696544
tarunp2912 <tarunpanchal42@gmail.com>
2025-04-01 15:07:12 +0530

    add code

--- src/index-2.php
+++ src/index-2.php
@@ -84,7 +84,7 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="index-2.php"><h3>E-Buspass</h3>
+                        <a class="navbar-brand" href="index-2.php"><h3>E-Buspass available here!</h3>
                     </div>
                     <div id="navbar" class="navbar-collapse collapse navbar-right navigation-holder">
                         <button class="close-navbar"><i class="fa fa-close"></i></button>
